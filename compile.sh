3801e136a68a17fbc1d4947a7958a7bfdcc4dcec
kanon-ify <ributkukuh270@gmail.com>
2024-02-01 09:32:12 +0700

    compile.sh: update script from zetabyte

--- compile.sh
+++ compile.sh
@@ -1,43 +1,58 @@
 #!/bin/bash
+# copy right by zetaxbyte
+# you can rich me on telegram t.me/@zetaxbyte
 
-echo -e "==========================="
-echo -e "= START COMPILING KERNEL  ="
-echo -e "==========================="
-bold=$(tput bold)
-normal=$(tput sgr0)
+cyan="\033[96m"
+green="\033[92m"
+red="\033[91m"
+blue="\033[94m"
+yellow="\033[93m"
 
-# Scrip option
-while (( ${#} )); do
-    case ${1} in
-        "-Z"|"--zip") ZIP=true ;;
-    esac
-    shift
-done
-[[ -z ${ZIP} ]] && { echo "${bold}LOADING-_-....${normal}"; }
+echo -e "$cyan===========================\033[0m"
+echo -e "$cyan= START COMPILING KERNEL  =\033[0m"
+echo -e "$cyan===========================\033[0m"
+
+echo -e "$blue...LOADING...\033[0m"
+
+echo -e -ne "$green## (10%\r"
+sleep 0.7
+echo -e -ne "$green#####                     (33%)\r"
+sleep 0.7
+echo -e -ne "$green#############             (66%)\r"
+sleep 0.7
+echo -e -ne "$green#######################   (100%)\r"
+echo -ne "\n"
+
+echo -e -n "$yellow\033[104mPRESS ENTER TO CONTINUE\033[0m"
+read P
+echo  $P
+
+# change DEFCONFIG to you are defconfig name or device codename
 
 DEFCONFIG="tama_aurora_defconfig"
-export KBUILD_BUILD_USER=Aurora
+
+# you can set you name or host name(optional)
+
+export KBUILD_BUILD_USER=To.Infinity.And.Beyond
 export KBUILD_BUILD_HOST=kanonify
-TC_DIR="/home/kanonify/clang-17"
+
+# change TC_DIR(directory) with your clang
+
+TC_DIR="/"
+
+# do not modify export PATCH it's been including with TC_DIR
+
 export PATH="$TC_DIR/bin:$PATH"
 
-if [[ $1 = "-r" || $1 = "--regen" ]]; then
-make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-cp out/defconfig arch/arm64/configs/$DEFCONFIG
-exit
-fi
-
-rm -R out
 mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
-
 make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- 2>&1 | tee log.txt
 
-    echo -e "==========================="
-    echo -e "   COMPILE KERNEL COMPLETE "
-    echo -e "==========================="
-
-if [[ ":v" ]]; then
-exit
+if [ -f out/arch/arm64/boot/Image.gz ] ; then
+    echo -e "$cyan===========================\033[0m"
+    echo -e "$cyan=  SUCCESS COMPILE KERNEL =\033[0m"
+    echo -e "$cyan===========================\033[0m"
+else
+echo -e "$red!ups...something wrong!?\033[0m"
 fi
